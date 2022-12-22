05675649bca2f062a0d5ea322bcfe635df9431cf
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-22 23:09:25 +0000

    get all  api info

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,7 +8,7 @@ const getApiInfo = async () => {
 	let allData = [];
 	for (let i = 0; i <= 41; i++) {
 		const res = await axios.get(`${apiLink}/character`);
-		const data = res.data.results;
+		// const data = res.data.results;
 		allData = [...allData, ...data];
 	}
 	// console.log(allData[0]);
