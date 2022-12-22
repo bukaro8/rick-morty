89fa933bb56c0cbe2b591cc1b859f9cd63037f8a
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-22 23:07:50 +0000

    db connection

--- api/src/db.js
+++ api/src/db.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-// const Character = require('./models/Character');
-// const Episode = require('./models/Episode');
+
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const sequelize = new Sequelize(
 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/repasopi`,
